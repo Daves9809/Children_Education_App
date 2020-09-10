b8e5757262bf194ee7b76b710ff612e12f3a77a4
Dawid Seroka <seroka.dawid1@gmail.com>
2020-09-10 13:13:57 +0200

    Connecting with online database phpmyAdmin

---
+++ app/src/main/java/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package loginandregistration.app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://dawid.server802166.nazwa.pl/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://dawid.server802166.nazwa.pl/register.php";
+}
