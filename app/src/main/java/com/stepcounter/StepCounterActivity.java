9e1792f95a2c72db208fbc8798fcb3f18d5e7928
Dawid Seroka <seroka.dawid1@gmail.com>
2020-09-17 22:36:02 +0200

    step-counter working also on s10e

--- app/src/main/java/com/stepcounter/StepCounterActivity.java
+++ app/src/main/java/com/stepcounter/StepCounterActivity.java
@@ -1,15 +1,19 @@
 package com.stepcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.loginandregistration.R;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,20 +24,28 @@ public class StepCounterActivity extends AppCompatActivity implements SensorEven
     private TextView textViewStepCounter, textViewStepDetector;
     private SensorManager mSensorManager;
     private boolean running = false;
-    int stepCount = 0;
-
+    int stepCount = 0, stepDetect = 0;
+    private Sensor countSensor, detectorSensor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_counter);
+        // permission to app working propely also on s10e
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION},1);
+        }
 
         textViewStepCounter = (TextView) findViewById(R.id.textViewStepCounter);
-        textViewStepDetector = findViewById(R.id.textViewStepDetector);
+        textViewStepDetector = (TextView) findViewById(R.id.textViewStepDetector);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        countSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        detectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
 
     }
@@ -42,28 +54,34 @@ public class StepCounterActivity extends AppCompatActivity implements SensorEven
     protected void onResume() {
         super.onResume();
         running = true;
-        Sensor countSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if(countSensor != null){
-            mSensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
+        if (countSensor != null) {
+            mSensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
             Toast.makeText(this, "Sensor found", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(this,"Sensor not found",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
         }
+        if (detectorSensor != null) {
+            mSensorManager.registerListener(this, detectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            Toast.makeText(this, "Detector Sensor found", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Detector Sensor not found", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        running = false;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(running){
             stepCount = (int) event.values[0];
             textViewStepCounter.setText(String.valueOf(stepCount));
-        }
-
+            if(stepCount == 400) {
+                stepCount = 0;
+                Toast.makeText(this, "Na dzis koniec", Toast.LENGTH_SHORT).show();
+            }
     }
 
     @Override
